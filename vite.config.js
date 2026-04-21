313e0ba9c141081d44d6b7f4980cb97c3af46d59
João Vitor Coutinho <coutinhoasdf@gmail.com>
2026-04-21 00:18:38 -0300

    Alterando caminhos do site

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PORTILOFIO-JVC-REACT /',
+  base: '/PORTILOFIO-JVC-REACT/',
 })
